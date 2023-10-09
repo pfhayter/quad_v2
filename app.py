135c5b81863d78deaff659f2ce45cb1b7ba5ad44
pfhayter <paul.hayter@gmail.com>
2023-10-09 11:33:02 -0600

    Init Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify, send_from_directory
+from PIL import Image
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads/'
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file:
+        filename = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filename)
+        split_image(filename)
+        return jsonify({'success': True}), 200
+
+@app.route('/image/<filename>', methods=['GET'])
+def serve_image(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+def split_image(filepath):
+    with Image.open(filepath) as img:
+        width, height = img.size
+        quadrant_size = (width // 2, height // 2)
+        
+        # Top-left quadrant
+        tl = img.crop((0, 0, quadrant_size[0], quadrant_size[1]))
+        tl.save(os.path.join(UPLOAD_FOLDER, 'tl_' + os.path.basename(filepath)))
+        
+        # Top-right quadrant
+        tr = img.crop((quadrant_size[0], 0, width, quadrant_size[1]))
+        tr.save(os.path.join(UPLOAD_FOLDER, 'tr_' + os.path.basename(filepath)))
+
+        # Bottom-left quadrant
+        bl = img.crop((0, quadrant_size[1], quadrant_size[0], height))
+        bl.save(os.path.join(UPLOAD_FOLDER, 'bl_' + os.path.basename(filepath)))
+
+        # Bottom-right quadrant
+        br = img.crop((quadrant_size[0], quadrant_size[1], width, height))
+        br.save(os.path.join(UPLOAD_FOLDER, 'br_' + os.path.basename(filepath)))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,39 @@
+document.getElementById("uploadForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let formData = new FormData(this);
+
+    fetch('../uploads', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            let filenames = ['tl_', 'tr_', 'bl_', 'br_'];
+            let previewGrid = document.getElementById("previewGrid");
+            previewGrid.innerHTML = ''; // Clear previous images
+
+            filenames.forEach(prefix => {
+                let slideElement = document.createElement("div");
+                slideElement.className = "swiper-slide";
+                
+                let imgElement = document.createElement("img");
+                imgElement.src = "/image/" + prefix + formData.get("file").name;
+                
+                slideElement.appendChild(imgElement);
+                previewGrid.appendChild(slideElement);
+            });
+            
+            // Initialize Swiper after adding the images
+            new Swiper('.swiper-container', {
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        }
+    })
+    .catch(error => {
+        console.error("Error uploading image:", error);
+    });
+});
